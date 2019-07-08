18464096d5454de1e0753505ef8c120602afcdba
Katie Mishra <katiemishra@fb.com>
2019-07-08 15:32:09 -0700

    user can logout

--- app/src/main/java/com/codepath/instagram/Home.java
+++ app/src/main/java/com/codepath/instagram/Home.java
@@ -1,14 +1,32 @@
 package com.codepath.instagram;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.ParseUser;
+
 public class Home extends AppCompatActivity {
+
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        logout = findViewById(R.id.btnLogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+                Intent homeToLogin = new Intent(Home.this, MainActivity.class);
+                startActivity(homeToLogin);
+            }
+        });
     }
 }
