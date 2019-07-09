91931d5d7380a98ceb1a7860017b461cfdaccaae
Katie Mishra <katiemishra@fb.com>
2019-07-08 17:16:11 -0700

    user can take a picture and post to instagram

--- app/src/main/java/com/codepath/instagram/ParseApp.java
+++ app/src/main/java/com/codepath/instagram/ParseApp.java
@@ -3,11 +3,15 @@ package com.codepath.instagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
         final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
                 .applicationId("katiemishra-instagram")
                 .clientKey("katiemishra-instagram")
